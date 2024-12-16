997fe7442bfbca70736be500f1b32da1772a443a
Juan Muñoz <cl.jmunoz@gmail.com>
2024-12-15 22:38:17 -0300

    secret data add

--- api_eresto/urls.py
+++ api_eresto/urls.py
@@ -39,7 +39,8 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('redocs/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
     path('api/', include('users.api.router')),
     path('api/', include(router_user.urls)),
 ]
